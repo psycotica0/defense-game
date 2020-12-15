32c1d85aef1bc8106f46639d4ec655d7abc18f68
Christopher Vollick <0@psycoti.ca>
2020-12-15 12:01:08 -0500

    Don't Duplicate Wires
    This is dead simple, but it works for now.
    I make a big map of all wires by their "wire coords", and if there's already a
    wire there, then don't make a new one.
    
    This will become more important later when I'm maintaining connections and
    modifying the routing, etc.

--- test level.gd
+++ test level.gd
@@ -1,29 +1,37 @@
 extends Spatial
 
 var wire_scene = preload("res://Wires.tscn")
+
+var allWires = {}
  
 func _ready():
 	pass
 
 func addWire(pos, normal):
-	var new_wires = wire_scene.instance()
-	var OFFSET = Vector3(5, 5, 5)
-	get_tree().root.add_child(new_wires)
-	var tile_position = pos * 10 + OFFSET
-	new_wires.translation = tile_position
-	match normal:
-		Vector3.UP: # This is floor, do nothing
-			pass
-		Vector3.DOWN: # Ceiling
-			new_wires.rotate_x(PI)
-		Vector3.LEFT: # This is wall
-			new_wires.rotate_z(PI/2)
-		Vector3.RIGHT:
-			new_wires.rotate_z(-PI/2)
-		Vector3.FORWARD:
-			new_wires.rotate_x(-PI/2)
-		Vector3.BACK:
-			new_wires.rotate_x(PI/2)
+	var existing = allWires.get([pos, normal])
+	if existing:
+		print("Existing")
+	else:
+		print("New one")
+		var new_wires = wire_scene.instance()
+		allWires[[pos, normal]] = new_wires
+		var OFFSET = Vector3(5, 5, 5)
+		get_tree().root.add_child(new_wires)
+		var tile_position = pos * 10 + OFFSET
+		new_wires.translation = tile_position
+		match normal:
+			Vector3.UP: # This is floor, do nothing
+				pass
+			Vector3.DOWN: # Ceiling
+				new_wires.rotate_x(PI)
+			Vector3.LEFT: # This is wall
+				new_wires.rotate_z(PI/2)
+			Vector3.RIGHT:
+				new_wires.rotate_z(-PI/2)
+			Vector3.FORWARD:
+				new_wires.rotate_x(-PI/2)
+			Vector3.BACK:
+				new_wires.rotate_x(PI/2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
