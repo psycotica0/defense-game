58abe99ff8cda353d494d8387a730ec04a985181
Christopher Vollick <0@psycoti.ca>
2020-12-25 13:59:46 -0500

    Turret Firing
    I may have overcomplicated things a bit, but I think I'm still ok.
    The gun now has a state machine that goes through various states.
    
    This means the gun now requires power and turns off without it, and after it
    locks on to something it begins to fire, which in this case is just a red
    cylinder which is made visible.
    
    There's definitely some duplication, and, like I said, perhaps too much state
    machine, but I think this is right.
    
    Later I'll add heat and the venting state, but in that case I think this will
    be helpful.

--- Turret.gd
+++ Turret.gd
@@ -10,40 +10,105 @@ onready var belowCast = $"Gun Holder/BelowCast"
 var scanOrder = []
 
 onready var gunHolder = $"Gun Holder"
+onready var laser = $"Gun Holder/Barrel/Laser"
 
 var source
 var circuit
+var demand = 15
 
 var currentTarget
+var currentState = State.OFF
+
+enum State {OFF, SCANNING, TARGETTING, FIRING, VENTING}
 
 func _ready():
 	scanOrder = [barrelCast, aboveCast, belowCast, topCast]
-	for ray in scanOrder:
-		ray.enabled = true
+	changeState(State.OFF)
 
 func changeCircuit(newCircuit):
+	var oldCircuit = circuit
 	circuit = newCircuit
+	if circuit:
+		circuit.connect("power_updated", self, "circuitPowerChanged")
+		circuit.addSink(self)
+	else:
+		changeState(State.OFF)
+		if oldCircuit:
+			oldCircuit.disconnect("power_updated", self, "circuitPowerChanged")
+			oldCircuit.removeSink(self)
+
+func changeState(newState):
+	match newState:
+		State.OFF, State.VENTING:
+			laser.visible = false
+			currentTarget = null
+			gunHolder.rotation.x = deg2rad(-20)
+			for ray in scanOrder:
+				ray.enabled = false
+		State.SCANNING:
+			laser.visible = false
+			currentTarget = null
+			gunHolder.rotation.x = 0
+			for ray in scanOrder:
+				ray.enabled = true
+		State.TARGETTING, State.FIRING:
+			laser.visible = false
+			for ray in scanOrder:
+				ray.enabled = true
+	currentState = newState
+
+func circuitPowerChanged(s_circuit, power):
+	if s_circuit == circuit:
+		if circuit.capacity == 0:
+			changeState(State.OFF)
+		elif currentState == State.OFF:
+			changeState(State.SCANNING)
 
 func _physics_process(delta):
-	var previousTarget = currentTarget
-	currentTarget = null
-	# Check Rays
-	# My logic here is that I'll set current target to the first thing my
-	# rays see, in the priority order I've listed them, unless any of them see
-	# the target I had last update. That takes ultimate priority
+	match currentState:
+		State.OFF:
+			pass
+		State.SCANNING:
+			processScanning(delta)
+		State.TARGETTING:
+			processTargetting(delta)
+		State.FIRING:
+			processFiring(delta)
+		State.VENTING:
+			pass
+
+func processFiring(delta):
+	if not currentTarget:
+		changeState(State.SCANNING)
+	elif barrelCast.get_collider() != currentTarget:
+		changeState(State.TARGETTING)
+	else:
+		# Keep us pointed at the center of our target
+		gunHolder.look_at(currentTarget.getAimTarget(), -source.normal)
+		laser.visible = true
+
+func processTargetting(delta):
+	if not currentTarget:
+		changeState(State.SCANNING)
+	elif barrelCast.get_collider() == currentTarget:
+		changeState(State.FIRING)
+	else:
+		var targetted = false
+		for ray in scanOrder:
+			if ray.get_collider() == currentTarget:
+				gunHolder.look_at(currentTarget.getAimTarget(), -source.normal)
+				targetted = true
+				break
+		if not targetted:
+			changeState(State.SCANNING)
+
+func processScanning(delta):
 	for ray in scanOrder:
 		var obj = ray.get_collider()
-		if obj:
-			if obj == previousTarget:
-				currentTarget = obj
-				break
-			elif not currentTarget and obj.has_method("getAimTarget"):
-				currentTarget = obj
+		if obj and obj.has_method("getAimTarget"):
+			currentTarget = obj
+			changeState(State.TARGETTING)
+			return
 	
-	if currentTarget:
-		# Do Shootsin
-		gunHolder.look_at(currentTarget.getAimTarget(), -source.normal)
-	else:
-		# Continue to scan
-		gunHolder.rotate_y((SCAN_RPM / 60.0) * TAU * delta)
-		gunHolder.rotation.x = 0
+	# Continue to scan
+	gunHolder.rotate_y((SCAN_RPM / 60.0) * TAU * delta)
