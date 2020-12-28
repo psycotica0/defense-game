1f91190900dbc664a456dc534f731008cb4b79ae
Christopher Vollick <0@psycoti.ca>
2020-12-28 15:48:56 -0500

    Merge Shields
    Not a tonne of code, now that we're here.
    I add my emitters to their list, and then tell them to respread.
    Then remove myself!

--- Shield.gd
+++ Shield.gd
@@ -25,6 +25,12 @@ func spreadShield():
 	for i in range(1, STRENGTH):
 		for child in target.get_children():
 			var neigh = child.getNeighbours()
+			for f in neigh:
+				var v = neigh[f]
+				if v and v != self and v.has_method("mergeShield"):
+					# If any of my neighbours is a different shield, then immediately stop here and merge with them
+					mergeShield(v)
+					return
 			if not neigh[ShieldPiece.Spot.POSZ]:
 				makePiece(child.transform.origin + Vector3(0, 0, 10), STRENGTH - i)
 			if not neigh[ShieldPiece.Spot.NEGZ]:
@@ -33,6 +39,15 @@ func spreadShield():
 				makePiece(child.transform.origin + Vector3(10, 0, 0), STRENGTH - i)
 			if not neigh[ShieldPiece.Spot.NEGX]:
 				makePiece(child.transform.origin + Vector3(-10, 0, 0), STRENGTH - i)
+
+func mergeShield(otherShield):
+	for emitter in emitters:
+		emitter.shield = otherShield
+	
+	otherShield.emitters += emitters
+	get_parent().remove_child(self)
+	otherShield.spreadShield()
+	self.queue_free()
 
 func makePiece(position, strength):
 	var newOne = template.duplicate()
